a636167051e870f6ba5ea3be53de1dc6abfa1816
jmfernandezfreige <202311871@alu.comillas.edu>
2026-05-05 11:11:19 +0200

    Añadir backend

---
+++ Backend_casaInteligente/src/main/java/spring/backend/casaInteligente/CasaInteligenteApplication.java
@@ -0,0 +1,13 @@
+package spring.backend.casaInteligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaInteligenteApplication.class, args);
+	}
+
+}
---
+++ Backend_casaInteligente/src/test/java/spring/backend/casaInteligente/CasaInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.backend.casaInteligente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
